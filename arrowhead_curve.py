f06c0a5e67f7382f1c9782da1b24fb6f438806ed
Ndi7484 <ndimasekaeka10903@gmail.com>
2026-03-31 11:07:30 +0700

    test

--- arrowhead_curve.py
+++ arrowhead_curve.py
@@ -1,18 +1,20 @@
 import turtle as tr
 # tr.tracer(False)
 p=tr.Pen()
-def arrowhead(p,n=30,level=0):
+# p.speed(1)
+def arrowhead(p,n=30,level=0,right=False):
     if level==0:
         for i in range(3):
             p.fd(n)
-            p.lt(60)
+            p.lt(60) if not right else p.rt(60)
     else:
-        arrowhead(p,n,level-1)
-        p.rt(120)
         for i in range(3):
-            p.fd(n)
-            p.rt(60)
-        arrowhead(p,n,level-1)
+            arrowhead(p,n,level-1, True if i%2!=0 else False)
+            p.rt(120) if not right else p.lt(60)
+            for i in range(3):
+                p.fd(n)
+                p.rt(60) if not right else p.lt(60)
+            arrowhead(p,n,level-1, True if i%2!=0 else False)
 arrowhead(p,30,1)
 # tr.update()
 tr.done()
