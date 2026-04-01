d5421c5af3122dd8cb151e83d9b51cfa218d0ec0
Ndi7484 <ndimasekaeka10903@gmail.com>
2026-04-01 09:35:32 +0700

    arrowhead C

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from triangle import triangle_fractal
 from square import square_fractal
 from koch_curve import kochcurve_fractal
 from koch_snowflake import koch_snowflake_fractals
+from arrowhead_curve import sierpinski_arrowhead_curve_fractal
 
 print('MENU')
 print('='*35)
 print('add \'a\' on the end to use default option')
 print('='*35)
 n=1
-for i in ['Sierpiński triangle - origin','Sierpiński square - origin','Koch Curve','Koch Snowflake']:
+for i in ['Sierpiński triangle - origin','Sierpiński square - origin','Koch Curve','Koch Snowflake','Sierpiński Arrowhead Curve Triangle']:
   print(f'{n}. {i}')
   n+=1
 pilihan=input('select fractals :')
@@ -29,5 +30,9 @@ match pilihan:
     koch_snowflake_fractals(inserting=True)
   case '4a':
     koch_snowflake_fractals()
+  case '4':
+    sierpinski_arrowhead_curve_fractal(inserting=True)
+  case '4a':
+    sierpinski_arrowhead_curve_fractal()
   case _:
     print('no such selection')
